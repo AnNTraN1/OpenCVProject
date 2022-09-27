fab673c1ee93f14330ca546849d49e2f050a8c4f
AnNTraN1 <110753234+AnNTraN1@users.noreply.github.com>
2022-09-27 23:43:42 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <sstream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+using namespace cv;
+using namespace std;
+//const char* params
+//= "{ help h         |           | Print usage }"
+//"{ input          | highway.mp4 | Path to a video or a sequence of image }"
+//"{ algo           | MOG2      | Background subtraction method (KNN, MOG2) }";
+
+
+
+int main(int argc, char* argv[])
+{
+    VideoCapture cap;
+    Mat videoFarme;
+
+    cap.open(0);
+    namedWindow("VidepCapture", WINDOW_AUTOSIZE);
+
+    /*VideoCapture cap(samples::findFile("vtest.avi"));*/
+
+    //string image_path = samples::findFile("C:/Users/ttien/Downloads/fb1.jpg");
+    //
+    //Mat img = imread(image_path, IMREAD_COLOR);
+
+   
+
+    Ptr<BackgroundSubtractor> pBackSub;
+        pBackSub = createBackgroundSubtractorMOG2();
+        /*pBackSub = createBackgroundSubtractorKNN();*/
+    
+    Mat frame, fgMask;
+    while (true) {
+        cap >> frame;
+        if (frame.empty())
+            break;
+       pBackSub->apply(frame, fgMask);
+        imshow("Frame", frame);
+        imshow("FG Mask", fgMask);
+        int keyboard = waitKey(30);
+        if (keyboard == 'q' || keyboard == 27)
+            break;
+    }
+    return 0;
+}
